d263fd02d416806d0308e6633c1bd00cf5db5137
JakeKazu <kazu9322@gmail.com>
2018-01-17 20:34:44 +0900

    コメントを修正しました。

--- models/scrape.rb
+++ models/scrape.rb
@@ -33,11 +33,10 @@ def get_subjects
     return e
   end
 
-  # スクレイピング先のURL
+  # スクレイピング
   url = 'http://www.kougi.shimane-u.ac.jp/selectweb/conduct_list.asp'
   agent = Mechanize.new
   subjects = Array.new()
-
   (1..100).each do |i|
     response = agent.post(url,{ abspage: i })
     break if i.to_s != response.forms[0].field_with(:name => 'abspage').value
@@ -48,6 +47,5 @@ def get_subjects
       subjects << Subject.new(subject) if studies.index(subject[4]) != nil
     end
   end
-
   subjects
 end
